4286cdc1a9487dd6a16a18a3e7222da6dae55fb1
ZHANG.JJ <5616760@qq.com>
2019-05-27 14:58:51 +0800

    自动生成柱网

---
+++ GridPractise/GridTest.cs
@@ -0,0 +1,61 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GridPractise {
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class GridTest : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+            FilteredElementCollector coll = new FilteredElementCollector(doc);
+            ElementClassFilter gridFilter = new ElementClassFilter(typeof(Grid));
+            List<Element> grid = coll.WherePasses(gridFilter).ToElements().ToList();
+            List<Line> gridLines = new List<Line>();
+            List<XYZ> intXyzs = new List<XYZ>();
+            foreach (Grid g in grid) {
+                gridLines.Add(g.Curve as Line);
+            }
+
+            foreach (Line line1 in gridLines) {
+                foreach (Line line2 in gridLines) {
+                    XYZ normal1 = line1.Direction;
+                    XYZ normal2 = line2.Direction;
+                    if (normal1.IsAlmostEqualTo(normal2)) {
+                        continue;
+                    }
+
+                    SetComparisonResult intRst = line1.Intersect(line2, out IntersectionResultArray results);
+                    if (intRst == SetComparisonResult.Overlap && results.Size == 1) {
+                        XYZ tp = results.get_Item(0).XYZPoint;
+                        if (intXyzs.Where(m => m.IsAlmostEqualTo(tp)).Count() == 0) {
+                            intXyzs.Add(tp);
+                        }
+                    }
+                }
+            }
+
+            Level level = doc.GetElement(new ElementId(311)) as Level;
+            FamilySymbol familySymbol = doc.GetElement(new ElementId(338370)) as FamilySymbol;
+            using (Transaction tr = new Transaction(doc)) {
+                tr.Start("Clomn");
+                if (!familySymbol.IsActive) {
+                    familySymbol.Activate();
+                }
+
+                foreach (XYZ xyz in intXyzs) {
+                    FamilyInstance familyInstance =
+                        doc.Create.NewFamilyInstance(xyz, familySymbol, level, StructuralType.NonStructural);
+                }
+
+                tr.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ SolidPractise/SolidTest.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SolidPractise {
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class SolidTest : IExternalCommand {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+            ICollection<ElementId> ids = uiDoc.Selection.GetElementIds();
+            Element selElem = uiDoc.Document.GetElement(ids.First());
+            GeometryElement ge = selElem.get_Geometry(new Options());
+            double area = 0;
+            double volume = 0;
+            int triangleCount = 0;
+            foreach (GeometryObject o in ge) {
+                if (o is Solid) {
+                    Solid sd = o as Solid;
+                    foreach (Face face in sd.Faces) {
+                        area += face.Area * 0.3048 * 0.3048;//这里计算了所有的面，所以和特性中显示的不一致。
+                        Mesh mesh = face.Triangulate(0.5);
+                        triangleCount += mesh.NumTriangles;
+                    }
+
+                    volume += sd.Volume * 0.3048 * 0.3048 * 0.3048;
+                }
+            }
+
+            TaskDialog.Show("计算", "面积总和为：" + area.ToString() + "平方米\n" +
+                                  "体积为：" + volume.ToString("0.000") + "立方米\n" +
+                                  "三角网格数为：" + triangleCount.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
